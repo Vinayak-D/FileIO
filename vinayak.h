9efdabbbb9f2a1041cb0c28e14faab5be083fa7b
Vinayak-Desh <vinayak.desh2@gmail.com>
2019-10-14 17:12:55 -0400

    first

---
+++ vinayak.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <iomanip> //used for formatting outputs
+// <iostream> means look in standard directory where all libraries are
+//in header files, can do things like include
+// then in main program, write include "headerfilename.h"
+
+//the string stream library
+#include <sstream>
+#include <vector>
+#include <fstream>
+using namespace std; 
+#pragma once
