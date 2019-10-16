07a2e87676626a75c77acb31dce8b7ef47115c0c
Vinayak-Desh <vinayak.desh2@gmail.com>
2019-10-16 16:52:30 -0400

    header done!

--- vinayak.h
+++ vinayak.h
@@ -4,10 +4,10 @@
 // <iostream> means look in standard directory where all libraries are
 //in header files, can do things like include
 // then in main program, write include "headerfilename.h"
-
 //the string stream library
 #include <sstream>
 #include <vector>
 #include <fstream>
 using namespace std; 
 #pragma once
+#define PI 3.1415;
